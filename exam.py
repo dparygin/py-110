1b564b1e4e768957b94dcca874dec109d3ef32b8
Dparygin <resss@mail.com>
2020-06-20 01:58:58 +0300

    update

--- exam.py
+++ exam.py
@@ -2,51 +2,25 @@ import json
 import random
 import re
 
-didict = {}
 english_pattern = re.compile(r'(([A-z])+-?\s?([A-z])+\b)')
 
 def import_data_json():##Проверка и получение пользовательских данных
-    try:
-        with open("ndtname.json", 'r') as read_file:
-            data = json.load(read_file)
-    except FileNotFoundError:
-        print("Файл пуст, создаем новый.")
-        data = input_data()
-        export_data_json(data)
-    return data
+    with open("ndtname.json", 'r') as read_file:
+        return json.load(read_file)
 
-def export_data_json(data):  # запись и переоткрытие внесенных данных
-    with open("ndtname.json", 'w') as savefile:
-        json.dump(data, savefile, indent=4)
-
-def input_data():  # Функция внесения данных
-    dictindex = {}
-    for key, value in ({'contry': 'страну', 'city': "город", 'street': "улицу"}).items():
-        listtrue = [] #Обнуляем список
-        listerorr = []
-        while len(listtrue) < 1:  # Цикл с проверкой заполнения
-            for word in input("Введите " + value + " (eng.) используя пробелы: ").split():
-                if not re.match(english_pattern, word):  # Проверка ввода
-                    listerorr.append(word)
-                else:
-                    listtrue.append(word)
-            # Отчет
-            if (len(listerorr) == 0) and (len(listtrue) > 0):
-                print("Добавлены все: ", listtrue)
-            elif (len(listerorr) > 0) and (len(listtrue) > 0):
-                print("Добавлено: ", len(listtrue), listtrue)
-                print("Отфильтровано: ", len(listerorr), listerorr)
-            else:
-                print("Ничего не добавлено:( \nВведите строку повторно.")
-            dictindex.update({key: listtrue})
-    for value in ('bild', 'kv'):
-        i1 = int(0)
-        listbild = []
-        while i1 < 9:
-            i1 = i1 + 1
-            listbild.append(random.randint(0, 100))
-        dictindex.update({value: listbild})
-    return dictindex
+def input_data(data):  # Проверка корректности данных
+    if not isinstance(dict, data):
+        return False
+    if len(data) >= 2:
+        return False
+    for key, listd in iter(data):
+        if not isinstance(list, listd):
+            return False
+        if len(listd) >= 2:
+        for word in listd:
+            if not re.match(english_pattern, word):
+                return False
+    return True
 
 def my_decorator(test):
     def dec(wrapped):
